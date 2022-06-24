85d6f2f3d40ef20a2820898797d491a16254cc8c
Tomi Prasetyo <tomiprasetyo95@gmail.com>
2022-06-24 17:02:53 +0700

    membuat API spec dan membuat schema database

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tomiprasetyo/belajar-restful-api-bunker-service
+
+go 1.18
---
+++ schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE bunker_services (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    guid VARCHAR(55) UNIQUE NOT NULL,
+    no_so VARCHAR(255) UNIQUE NOT NULL,
+    nama_perusahaan VARCHAR(255) NOT NULL,
+    nama_kapal VARCHAR(255) NOT NULL,
+    nama_produk VARCHAR(255) NOT NULL,
+    jumlah_pengisian INTEGER(55) NOT NULL,
+    pelabuhan VARCHAR(255) NOT NULL,
+    nopol_truk VARCHAR(255) NOT NULL,
+    nama_operator VARCHAR(255) NOT NULL,
+    description TEXT,
+    created_at DATETIME,
+    updated_at DATETIME
+);
